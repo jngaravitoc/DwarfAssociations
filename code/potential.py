988512b15b073a341ebb5c29f18f507d4edfc72b
jngaravitoc <jn.garavito57@uniandes.edu.co>
2014-02-20 17:18:41 -0500

    new code

--- code/potential.py
+++ code/potential.py
@@ -1,23 +1,35 @@
 import numpy as np
-import scipy as sp
 
 data = np.loadtxt("../data/B64_WM5_10909_LG_7Mpc_2048/data.txt")
 
-x = data[0:10,2]
-y = data[0:10,3]
-z = data[0:10,4]
-m = data[0:10,8]
+x = data[:,2]
+y = data[:,3]
+z = data[:,4]
+m = data[:,8]
 
 l = len(x)
 
-for i in range(2):
-    f = open("../data/Distances/distances"+str(i)+".txt", 'a')
-    f.write("Halo ID"+ "   " + "Distance(kpc)" + "  " + "Potencial" + "\n")
-    d = []
+xmin = min(x)
+xmax = max(x)
+
+ymin = min(y)
+ymax = max(y)
+
+zmin = min(z)
+zmax = max(z)
+
+f = open("../data/Distances/distances.txt", 'w')
+f.write("X(pc)"+ "   " + "Y(pc)" + "  " + "Z(pc)" + "  " + "Potencial" + "\n")
+    
+for i in range(l):
+    u = []
     for j in range(l):
         if i!=j:
             dist = np.sqrt((x[i]-x[j])**2 + (y[i]-y[j])**2 + (z[i]-z[j])**2)
             phi = m[i]*m[j]/dist
+            u.append(phi)
             #d.append(dist)
-            f.write(str(j)+ "  "+ str(dist)+ "  " + str(phi) + "\n")
+    U = sum(u)
+    f.write(str(x[i])+ "  "+ str(y[i])+ "  " + str(z[i]) + "  " + str(U) + "\n")
 f.close()
+
