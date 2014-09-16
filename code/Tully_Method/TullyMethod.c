2ee554c74ca437b27bba9afcba0b878f5eb6d5b7
jngaravitoc <nico.garavito@gmail.com>
2014-09-16 17:17:06 -0500

    minor update

--- code/Tully_Method/TullyMethod.c
+++ code/Tully_Method/TullyMethod.c
@@ -2,51 +2,83 @@
 #include <math.h>
 #include <stdlib.h>
 
-float *load_data(char *filename, int n_col);
+float  distances(float * x, float * y, float * z);
+float *load_data(char *filename, int n_col, float *x, float  * y, float * z, float * vx, float * vy, float * vz, float * m, float *Rvir );
 
-float *load_data(char *filename, int n_col){
+float *load_data(char *filename, int n_col, float * x, float * y, float * z, float * vx, float * vy, float * vz, float * m, float *Rvir ){
 //	float *data;
-	FILE *in;
-	float *x;
-	float *y;
-	float z;
-	float vx;
-	float vy;
-	float vz;
+        FILE *in;
+	float X;
+        float Y;
+	float Z;
+	float Vx;
+	float Vy;
+	float Vz;
 	float M;
-	float Rvir;
+	float RVir;
 	int i;	
-	
 
-	x = malloc(n_col * sizeof(double));
-	y = malloc(n_col * sizeof(double));
-	
+
+
 	in = fopen(filename, "r");	
 	if(!in){	
 	printf("Problem opening file %s\n",filename);
 	exit(1);
 
 }	for(i=0;i<=n_col;i++){
-	fscanf(in, "%f %f %f %f %f %f %f %f \n", x, y, &z, &vx, &vy, &vz, &M, &Rvir);
-	
-	//printf("%f %f %f %f %f %f %f %f \n", *x, y, z, vx, vy, vz, M, Rvir);
+	  fscanf(in, "%f %f  \n",&X, &Y);
+	  x[i]=X;
+	  y[i]=Y;
+	  z[i]=Z;
 }	
 	fclose(in);
-//	for(i=0;i<10;i++){
-//	printf("%f",*x);}
-	return x, y;
+	return x, y, z;
 }
 
 int main(){
-float *x;
-float *y;
+ float *x;
+ float *y;
+ float *z;
+ float *vx;
+ float *vy; 
+
+ float *vz;
+ float *m;
+ float *Rvir;
+ int i=11;
+ int n_points = 100;
+ float *D;
+ 
+ x = malloc(n_points*sizeof(double));
+ y = malloc(n_points*sizeof(double));
+ z = malloc(n_points*sizeof(double));
+ vx = malloc(n_points*sizeof(double));
+ vy = malloc(n_points*sizeof(double));
+ vz = malloc(n_points*sizeof(double));
+ m = malloc(n_points*sizeof(double));
+ Rvir = malloc(n_points*sizeof(double));
+ D = malloc(n_points*sizeof(double));
 
 
-x = malloc(100*sizeof(double));
-y = malloc(100*sizeof(double));
-x, y = load_data("/home/nicolas/Dropbox/github/DwarfAssociations/data/B64_WM5_10909_LG_7Mpc_2048/HR-data.txt", 14217);
+ x, y, z = load_data("/home/nicolas/Dropbox/github/DwarfAssociations/data/B64_WM5_10909_LG_7Mpc_2048/HR-data.txt", 100, x, y, z, vx, vy, vz, m, Rvir);
+  
+ D = distances(x, y, z);
+ 
+ printf("%f \n", D[1]);
 
-printf("%f %f", *x, *y);
-return 0;
+ //intf("%f %f\n",  x[1], y[1]);
+ return 0;
+
+}
+
+float distances(float *x, float *y, float *z){
+
+  float d;
+  int i; 
+
+  for(i=0;i<10;i++){
+  d[i] = pow(x[i],2) + pow(y[i],2) + pow(z[i],2);
+  }
+  return d;
 
 }
