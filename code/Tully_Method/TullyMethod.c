103871e03d047182262b7038f0649be549c0d982
jngaravitoc <nico.garavito@gmail.com>
2014-09-18 17:07:58 -0500

    center of mass computed

--- code/Tully_Method/TullyMethod.c
+++ code/Tully_Method/TullyMethod.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 
-void center_mass(float * m, float *D, float *cm, int n_points );
+float *center_mass(float * m, float *D, float *cm, int n_points );
 float *distances(float * x, float * y, float * z, float *D);
 float *load_data(char *filename, int n_col, float *x, float  * y, float * z, float * vx, float * vy, float * vz, float * m, float *Rvir );
 
@@ -65,7 +65,7 @@ int main(){
  m = malloc(n_points*sizeof(double));
  Rvir = malloc(n_points*sizeof(double));
  D = malloc(n_points*sizeof(double));
- cm = malloc(n_points*sizeof(double));
+ cm = malloc(n_points*(n_points-1)*sizeof(double));
 
  x, y, z = load_data("/home/nicolas/Dropbox/github/DwarfAssociations/data/B64_WM5_10909_LG_7Mpc_2048/HR-data.txt", 100, x, y, z, vx, vy, vz, m, Rvir);
  
@@ -73,9 +73,9 @@ int main(){
 
  distances(x, y, z, D);
  center_mass(m, D, cm, n_points);
- //for(i=0;i<n_points;i++){
- //printf("esto es D en el main %f \n", cm[i]);
- //}
+for(i=0;i<n_points;i++){
+ printf("esto es cm en el main %f \t %f  \n", cm[i], D[i]);
+}
  return 0;
 
 }
@@ -85,25 +85,27 @@ float  * distances(float *x, float *y, float *z, float *D){
   int i; 
    for(i=0;i<99;i++){
    D[i] = pow(x[i],2) + pow(y[i],2) + pow(z[i],2);
-    //printf("esto es d dentro del for %f \n", D[i]); //Esto funciona
-    //printf("Esto es x dentro del for %f \n", x[i]);
+   // printf("DDD %f \n", D[i]);
     }
   return D;
 }
 
 
-void center_mass(float *m, float *D, float *cm, int n_points){
+float *  center_mass(float *m, float *D, float *cm, int n_points){
   
   int i;
   int j;
-  printf("cm inicial %f ", cm[0]);
+  //printf("cm inicial %f ", cm[0]);
   for(i=0;i<n_points;i++){
     for(j=0;j<n_points;j++){
-      if(i!=j){
-	cm[i]=m[i]*pow(D[i],2) /(m[j]*pow(D[j],2)) + (m[j]*pow(D[j],2)/(m[i] + m[j])) - (m[i]*m[j]*pow(D[i]-D[j],2)/(pow((m[i]+m[j]),2)));
+      if(j!=i){
+	cm[i]=m[i]*pow(D[i],2) /(m[j]+m[i]) + (m[j]*pow(D[j],2)/(m[i] + m[j])) - (m[i]*m[j]*pow(D[i]-D[j],2)/(pow((m[i]+m[j]),2)));
+	//printf("Cm \t  %f \n", cm[i]);
       }
   }
   }
-
-
+  //for(i=0;i<n_points;i++){
+    //printf("Cm \t  %f \n", cm[i]);
+  //  } 
+ return cm;
 }
