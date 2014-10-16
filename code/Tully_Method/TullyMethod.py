77350ba6ab880bdda2efbfb956d62dce08de640b
jngaravitoc <jn.garavito57@uniandes.edu.co>
2014-10-16 14:15:00 -0500

    code running :)

--- code/Tully_Method/TullyMethod.py
+++ code/Tully_Method/TullyMethod.py
@@ -1,85 +1,90 @@
 import numpy as np
-from array import array 
+from array import array
 import sys
 
-filename = "../../data/B64_WM5_10909_LG_7Mpc_2048/HR-data.txt"
+filename = "HRbcku.txt"
 #filename2  = sys.argv[1]
+N = 99 # Numero de lineas inicial
 
-def reading_data(filename):
-
-   data = np.loadtxt(filename)
-
-   X = data[:, 0]
-   Y = data[:, 1]
-   Z = data[:, 2]
-   M = data[:, 6]
-
-   X = X[0:100]
-   Y = Y[0:100]
-   Z = Z[0:100]
-   M = M[0:100]
-
-   return X, Y, Z, M
+def reading(filename):
+  data = np.loadtxt(filename)
+  ID = data[:,0]
+  x = data[:,1]
+  y = data[:,2]
+  z = data[:,3]
+  M = data[:,4]
+  return ID, x, y, z, M
 
 
-def distance(filename):
-   X, Y, Z, M = reading_data(filename)
-   d = []
-   for i in range(len(X)):
-	dist = np.sqrt(X[i]**2 + Y[i]**2 + Z[i]**2) 
-        d.append(dist)
-   f = open("distances.txt", "w")
-   f.write("#Distances    VirialMass" + "\n")
-   for i in range(len(X)):
-        f.write(str(d[i]) + "  " + str(M[i]) + "  " + str(X[i]) + "   " + str(Y[i]) + "   " + str(Z[i]) + "\n")
-   f.close()
+def distances(filename):
+    ID, x, y, z, M = reading(filename)
+    r = []
+    for i in range(len(x)):
+        for j in range(len(y)):
+             r.append(np.sqrt((x[i]-x[j])**2 + (y[i]-y[j])**2)+(z[i]-z[j])**2)
+            #print "distancia de: ", i, "a: ", j, " = ", d
+    return ID, x, y, z, M, r
 
-def CM(filename2):
-   data = np.loadtxt(filename2)
-   r = data[:,0]
-   M = data[:,1]
-   r_cm2 = []
-   for i in range(len(M)):
-	for j in range(len(M)):
-                if j != i:
-			r_cm = (M[i]*r[i]**2/(M[j] + M[i])) + (M[j]*r[j]**2/(M[i] + M[j])) - (M[i]*M[j]*(r[i]-r[j])**2/(M[i]+M[j])**2) 
-			r_cm2.append(r_cm)
-                        #print "changos"
-   #for i in range(len(r_cm2)):
-   return r_cm2
-#distance(filename)
+def force(filename):
+    ID, x, y, z, M, r = distances(filename)
+    idp = []
+    idq = []
+    N = len(x)
+    R = []
+    F = []
+    MT = []
+    #print M, r
+    for p in range(N):
+       for q in range(N):
+            if p!=q:
+                for j in range(N):
+                    if ((j!=p) & (j!=q)):
+                        Mt = M[p] + M[q]
+                        r_cm = np.sqrt( (M[p] / (Mt) * r[j+p*N]**2 )+ (M[q] / (Mt) * r[j + q*N]**2) - (M[p]*M[q] / Mt**2 * r[q+p*N]**2) )
+                        R.append(r_cm)
+                        idp.append(p)
+                        idq.append(q)
+                        if Mt > M[j]:
+                            F.append(1/(r_cm**2/Mt))
+                            MT.append(Mt)
+                        elif M[j] > Mt:
+                            F.append(1/(r_cm**2/M[j]))
+                            MT.append(M[j])
+    index = np.where(F == min(F))
+    index =  index[0][1]
 
-def merging(filename2):
-   cm_R = CM("distances.txt")
-   print "hola"
-   data = np.loadtxt("distances.txt")
-   X = data[:,0]
-   Y = data[:,1]
-   longitud =  len(data)-1 # por el encabezado
-   index = np.where(cm_R==min(cm_R))
-   minimo =  np.amin(index)
-   halo_j = int(minimo/longitud) # Hallando el indice de la particula 2.
-   halo_i = minimo - (halo_j*longitud) # Hallando el indice de la particula 1. 
-   print np.sqrt(cm_R[minimo]),"posicion particula 1", halo_i, "posicion particula 2", halo_j
-   print len(X)
-   x = np.delete(X,[halo_i, halo_j])
-   y = np.delete(Y,[halo_i, halo_j])
-   XX = np.append(x, np.sqrt(min(cm_R))) 
-   YY = np.append(y, Y[halo_i]+Y[halo_j])
-   f = open("distances.txt", "w")
-   for i in range(len(XX)):
-   	f.write(str(XX[i]) + "  " + str(YY[i]) + "\n")
-   f.close()
-   merging_rate = len(XX)
-   return merging_rate
+    return idp[index], idq[index], MT[index], ID, x, y, z, M
 
-distance(filename)
-x = CM("distances.txt")
-#for i in range(2):
-#while x>4:
-#	x = merging("distances.txt")
-for i in range(100):
-	print x[i]
+def new_particle(filename):
+    id_p, id_q, new_M, ID, x, y, z, M = force(filename)
+    data = np.loadtxt(filename)
+    indexp = np.where(ID == id_p)
+    indexq = np.where(ID == id_q)
+    xp = x[indexp]
+    yp = y[indexp]
+    zp = z[indexp]
+    xq = x[indexq]
+    yq = y[indexq]
+    zq = z[indexq]
+    IDp = ID[indexp]
+    IDq = ID[indexq]
+    Mp = M[indexp]
+    Mq = M[indexq]
+    Mpq = Mp + Mq
+    xt = 1 / Mpq * (Mp*xp + Mq*xq)
+    yt = 1 / Mpq * (Mp*yp + Mq*yq)
+    zt = 1 / Mpq * (Mp*zp + Mq*zq)
+    X = np.array([N, xt, yt, zt, new_M])
+    if IDp > IDq:
+      data = np.delete(data, IDp, 0)
+      data = np.delete(data, IDq, 0)
+    else:
+      data = np.delete(data, IDq, 0)
+      data = np.delete(data, IDp, 0)
 
+    np.insert(data, N, X)
 
-
+    return  data, X
+data, X = new_particle(filename)
+print X
+print data
