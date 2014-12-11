052c1f729184718311e6ff758264b8f25340b924
jngaravitoc <jn.garavito57@uniandes.edu.co>
2014-12-11 12:41:50 -0500

    Force implemented

--- code/Tully_Method/TullyMethod.c
+++ code/Tully_Method/TullyMethod.c
@@ -12,15 +12,15 @@ TO DO:
 #include <stdlib.h>
 
 
-float *center_mass(float * m, float *D, float *cm, int n_points );
-float *distances(float * x, float * y, float * z, float *D);
-float *load_data(char *filename, int n_col, float *x, float  * y, float * z, float * vx, float * vy, float * vz, float * m, float *Rvir );
+void *center_mass(float *m, float *D, float *x, float *y, float *z, int n_points, float * mt, double * cm, float *F);
+void *distances(float * x, float * y, float * z, float *D, int n_points);
+void *load_data(char *filename, int n_col, float * x, float * y, float * z, float * vx, float * vy, float * vz, float * m);
 
-float *load_data(char *filename, int n_col, float * x, float * y, float * z, float * vx, float * vy, float * vz, float * m, float *Rvir ){
+void *load_data(char *filename, int n_col, float * x, float * y, float * z, float * vx, float * vy, float * vz, float * m ){
 //	float *data;
-        FILE *in;
+  FILE *in;
 	float X;
-        float Y;
+  float Y;
 	float Z;
 	float Vx;
 	float Vy;
@@ -37,7 +37,7 @@ float *load_data(char *filename, int n_col, float * x, float * y, float * z, flo
 	exit(1);
 
 }	for(i=0;i<=n_col;i++){
-	  fscanf(in, "%f %f %f %f %f %f %f %f \n",&X, &Y, &Z, &Vx, &Vy, &Vz, &M, &RVir);
+	  fscanf(in, "%f %f %f %f %f %f %f \n",&X, &Y, &Z, &Vx, &Vy, &Vz, &M);
 	  x[i]=X;
 	  y[i]=Y;
 	  z[i]=Z;
@@ -45,10 +45,10 @@ float *load_data(char *filename, int n_col, float * x, float * y, float * z, flo
 	  vy[i] = Vy;
 	  vz[i] = Vz;
 	  m[i] = M;
-	  Rvir[i] = RVir;
+	  //Rvir[i] = RVir;
 	}
 	fclose(in);
-	return x, y, z, vx, vy, vz, M;
+	//return x, y, z, vx, vy, vz, M;
 }
 
 int main(){
@@ -63,7 +63,9 @@ int main(){
  int i=11;
  int n_points = 100;
  float *D;
- float *cm;
+ double *cm;
+ float *mt;
+ float *F;
 
  x = malloc(n_points*sizeof(double));
  y = malloc(n_points*sizeof(double));
@@ -72,54 +74,64 @@ int main(){
  vy = malloc(n_points*sizeof(double));
  vz = malloc(n_points*sizeof(double));
  m = malloc(n_points*sizeof(double));
+ mt = malloc(n_points*sizeof(double));
  Rvir = malloc(n_points*sizeof(double));
- D = malloc(n_points*sizeof(double));
- cm = malloc(n_points*(n_points-1)*sizeof(double));
+ D = malloc(n_points*n_points*sizeof(double));
+ cm = malloc(n_points*n_points*(n_points-1)*sizeof(double));
+ F = malloc(n_points*n_points*(n_points-1)*sizeof(double));
 
- x, y, z, vx, vy, vz = load_data("/home/nicolas/Dropbox/github/DwarfAssociations/data/B64_WM5_10909_LG_7Mpc_2048/HR-data.txt", 100, x, y, z, vx, vy, vz, m, Rvir);
+ load_data("data.dat", 100, x, y, z, vx, vy, vz, m);
 
 
 
- distances(x, y, z, D);
- center_mass(m, D, cm, n_points);
-for(i=0;i<n_points;i++){
- printf("%f \t %f  \n", D[i], cm[i]);
-}
+ distances(x, y, z, D, n_points);
+ center_mass(m, D, x, y, z,  n_points, mt, cm, F);
+//for(i=0;i<n_points;i++){
+ //printf("%f  \t %f \t %f \n", D[i], cm[i], mt[i]);
+//}
  return 0;
 
 }
 
-float  * distances(float *x, float *y, float *z, float *D){
+void  * distances(float *x, float *y, float *z, float *D, int n_points){
 
   int i;
-   for(i=0;i<99;i++){
-     for(j = 0; j<99;j++){
+  int j;
+   for(i=0;i<n_points;i++){
+     for(j = 0; j<n_points;j++){
        D[i] = pow(pow(x[i]-x[j],2) + pow(y[i]-y[j],2) + pow(z[i]-z[j],2), 0.5);
       }
     }
-  return D;
+
 }
 
 
-float *  center_mass(float *m, float *D, float *x, float *y, float *z, int n_points){
+void * center_mass(float *m, float *D, float *x, float *y, float *z, int n_points, float * mt, double * cm, float *F){
 
   int p;
   int q;
   int j;
+  //float M;
+  int i;
   for(p=0;p<n_points;p++){
     for(q=0;q<n_points;q++){
       if(p!=q){
         for(j=0;j<n_points;j++){
             if(j!=q & j!=p){
-              mt = M[q] + M[p]
-              cm[i]=pow(m[p]*pow(D[j+p*n_points],2) /mt + m[q]*pow(D[j+q*n_points],2) /mt - m[q]*m[p]*pow(D[q+p*n_points,2])/pow(mt,2),0.5);
+              mt[j] = m[q] + m[p];
+              cm[j]=m[p]*pow(D[j+p*n_points],2) / mt[j] + m[q]*pow(D[j+q*n_points],2) / mt[j] - m[q]*m[p]*pow(D[q+p*n_points],2) / pow(mt[j],2);
+              printf("%f \n", cm[j]);
+              if(mt[j] > m[j]){
+              F[j] = mt[j]/pow(cm[j],2);
+                }
+              else{
+              F[j] = m[j]/pow(cm[j],2);
+
+                }
               }
+
             }
-        }
+          }
         }
       }
-  }
-  }
-
- return cm;
 }
