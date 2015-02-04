8f00713518f194e6bb9bbfb43e2b012642c89589
jngaravitoc <jn.garavito57@uniandes.edu.co>
2015-02-04 08:31:15 -0500

    finding the error

--- code/Tully_Method/TullyMethod.c
+++ code/Tully_Method/TullyMethod.c
@@ -90,13 +90,13 @@ int main(){
 
 
 
- distances(x, y, z, D, n_points);
- center_mass(m, D, x, y, z,  n_points, mt, cm, F);
+distances(x, y, z, D, n_points);
+center_mass(m, D, x, y, z,  n_points, mt, cm, F);
  //printf("%f \n", D[0]);
-// for(i=0;i<n_points*n_points;i++){
-// printf("%f \t %f \n", D[i], cm[i]);
+// for(i=0;i<n_points;i++){
+// printf("%f \t %f \t %f \n", x[i], y[i], z[i]);
  //printf("%f  \t %f \t %f \n", D[i], cm[i], mt[i]);
-// }
+ //}
  return 0;
 
 }
@@ -109,7 +109,7 @@ void  * distances(float *x, float *y, float *z, float *D, int n_points){
   for(i=0;i<n_points;i++){
      for(j = 0; j<n_points;j++){
        D[i] = pow(pow(x[i]-x[j],2) + pow(y[i]-y[j],2) + pow(z[i]-z[j],2), 0.5);
-	//printf("%f \n", D[i]);
+       printf("%f \n", D[i]);
       }
     }
 
@@ -133,7 +133,7 @@ void * center_mass(float *m, float *D, float *x, float *y, float *z, int n_point
 	      n++;
               mt[j] = m[q] + m[p];
               cm[j] = m[p]*pow(D[j+p*n_points],2) / mt[j] + m[q]*pow(D[j+q*n_points],2) / mt[j] - m[q]*m[p]*pow(D[q+p*n_points],2) / pow(mt[j],2);
-              //printf("%f \t %d \t %d\n", cm[j], p, q);
+              printf("%f \t %d \t %d \t %d \t %f \t %f \t %f \t %f \t %f \n", cm[j], p, q, j, m[p], m[q], mt[j], D[j+p*n_points], D[q+p*n_points]);
 
               if(mt[j] > m[j]){
               F[j] = mt[j]/pow(cm[j],2);
@@ -142,7 +142,7 @@ void * center_mass(float *m, float *D, float *x, float *y, float *z, int n_point
               F[j] = m[j]/pow(cm[j],2);
 	      
                 }
-              printf("%f \t %d \t %d \t %d \t %f \t %f \t %f \t %f \t %f \t %f \n", F[j], p, q, j, m[p], m[q], mt[j], pow(D[j+p*n_points], 2), pow(D[j+q*n_points], 2), pow(D[q+p*n_points], 2));
+             //printf("%f \t %d \t %d \t %d \t %f \t %f \t %f \t %f \t %f \t %f \n", F[j], p, q, j, m[p], m[q], mt[j], pow(D[j+p*n_points], 2), pow(D[j+q*n_points], 2), pow(D[q+p*n_points], 2));
 	      /*if(n>0){
               // this is to print just the minimum values of F and to ignore distances of the same particle
 	      if(F[j]<F[j-1] & (D[j+p*n_points] !=0) & (D[j+q*n_points] != 0)){
