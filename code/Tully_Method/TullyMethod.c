4b52fa419370192ea15c96bc43059f37b49d5add
jngaravitoc <jn.garavito57@uniandes.edu.co>
2015-02-09 20:57:45 -0500

    good coordinates

--- code/Tully_Method/TullyMethod.c
+++ code/Tully_Method/TullyMethod.c
@@ -1,4 +1,7 @@
 /*
+
+This code was written by J. N. Garavito-Camargo in February of 2015.
+
 TO DO:
 1- Check Cm and F results from center_mass Fucking problem with distances
 2- Write merging function
@@ -13,14 +16,14 @@ TO DO:
 
 // ##########################         Defining functions            #######################
 
-void *center_mass(float *m, float *D, float *x, float *y, float *z, int n_points, float * mt, double * cm, float *F);
+void *center_mass(float *m,  float * D, float *x, float *y, float *z, int n_points, float * mt, double * cm, float *F);
 void *distances(float * x, float * y, float * z, float *D, int n_points);
-void *load_data(char *filename, int n_col, float * x, float * y, float * z, float * vx, float * vy, float * vz, float * m);
+void *load_data(char *filename, int n_col, float *x,  float * y, float * z, float * vx, float * vy, float * vz, float * m);
 
 // ##########################         Function that calls the data  #######################
 
 
-void *load_data(char *filename, int n_col, float * x, float * y, float * z, float * vx, float * vy, float * vz, float * m ){
+void *load_data(char *filename, int n_points, float * x, float * y, float * z, float * vx, float * vy, float * vz, float * m ){
         FILE *in;
 	float X;
   	float Y;
@@ -29,10 +32,7 @@ void *load_data(char *filename, int n_col, float * x, float * y, float * z, floa
 	float Vy;
 	float Vz;
 	float M;
-	float RVir;
 	int i;
-
-
 
 	in = fopen(filename, "r");
 	if(!in){
@@ -40,19 +40,18 @@ void *load_data(char *filename, int n_col, float * x, float * y, float * z, floa
 	exit(1);
 		}
 
-	for(i=0;i<=n_col;i++){
-	  fscanf(in, "%f %f %f %f %f %f %f \n",&X, &Y, &Z, &Vx, &Vy, &Vz, &M);
-	  x[i]=X;
-	  y[i]=Y;
-	  z[i]=Z;
-	  vx[i] = Vx;
-	  vy[i] = Vy;
-	  vz[i] = Vz;
-	  m[i] = M;
-	  //Rvir[i] = RVir;
+	for(i=0;i<=n_points;i++){
+	fscanf(in, "%f %f %f %f %f %f %f \n",&X, &Y, &Z, &Vx, &Vy, &Vz, &M);
+	x[i] = X; 
+        y[i] = Y;
+        z[i] = Z;
+        vx[i] = Vx;
+        vy[i] = Vy;
+        vz[i] = Vz;
+        m[i] = M;
 	}
 	fclose(in);
-	//return x, y, z, vx, vy, vz, M;
+	//return X, Y, Y, Vx, Vy, Vz, M;
 	}
 
 // ######################## Main function ################################
@@ -89,14 +88,16 @@ int main(){
  load_data("test.txt", n_points, x, y, z, vx, vy, vz, m);
 
 
-
-distances(x, y, z, D, n_points);
-center_mass(m, D, x, y, z,  n_points, mt, cm, F);
+ distances(x, y, z, D, n_points);
+ for(i=0;i<100;i++){
+ printf("en el main%f \n", D[1]);
+}
+ //center_mass(m, D, x, y, z,  n_points, mt, cm, F);
  //printf("%f \n", D[0]);
-// for(i=0;i<n_points;i++){
-// printf("%f \t %f \t %f \n", x[i], y[i], z[i]);
+// for(i=0;i<n_points*n_points;i++){
+// printf("%f \t %f \n", D[i], cm[i]);
  //printf("%f  \t %f \t %f \n", D[i], cm[i], mt[i]);
- //}
+// }
  return 0;
 
 }
@@ -105,11 +106,12 @@ void  * distances(float *x, float *y, float *z, float *D, int n_points){
 
   int i;
   int j;
+  int k = n_points*n_points;
 
   for(i=0;i<n_points;i++){
      for(j = 0; j<n_points;j++){
-       D[i] = pow(pow(x[i]-x[j],2) + pow(y[i]-y[j],2) + pow(z[i]-z[j],2), 0.5);
-       printf("%f \n", D[i]);
+       D[k] = pow(pow(x[i]-x[j],2) + pow(y[i]-y[j],2) + pow(z[i]-z[j],2), 0.5);
+       printf("%f \n", D[k]);
       }
     }
 
@@ -123,7 +125,9 @@ void * center_mass(float *m, float *D, float *x, float *y, float *z, int n_point
   int j;
   int i;
   int n;
-
+  for(i=0;i<100;i++){
+  printf("%f \n", D[i]);
+}
   for(p=0;p<n_points;p++){
     for(q=0;q<n_points;q++){
       if(p!=q){
@@ -133,7 +137,7 @@ void * center_mass(float *m, float *D, float *x, float *y, float *z, int n_point
 	      n++;
               mt[j] = m[q] + m[p];
               cm[j] = m[p]*pow(D[j+p*n_points],2) / mt[j] + m[q]*pow(D[j+q*n_points],2) / mt[j] - m[q]*m[p]*pow(D[q+p*n_points],2) / pow(mt[j],2);
-              printf("%f \t %d \t %d \t %d \t %f \t %f \t %f \t %f \t %f \n", cm[j], p, q, j, m[p], m[q], mt[j], D[j+p*n_points], D[q+p*n_points]);
+              //printf("%f \t %d \t %d\n", cm[j], p, q);
 
               if(mt[j] > m[j]){
               F[j] = mt[j]/pow(cm[j],2);
@@ -142,7 +146,7 @@ void * center_mass(float *m, float *D, float *x, float *y, float *z, int n_point
               F[j] = m[j]/pow(cm[j],2);
 	      
                 }
-             //printf("%f \t %d \t %d \t %d \t %f \t %f \t %f \t %f \t %f \t %f \n", F[j], p, q, j, m[p], m[q], mt[j], pow(D[j+p*n_points], 2), pow(D[j+q*n_points], 2), pow(D[q+p*n_points], 2));
+              printf("%f \t %d \t %d \t %d \t %f \t %f \t %f \t %f \t %f \t %f \t %d \t %d \n", F[j], p, q, j, m[p], m[q], mt[j], pow(D[j+p*n_points], 2), pow(D[j+q*n_points], 2), pow(D[q+p*n_points], 2), q+p*n_points, p+q*n_points);
 	      /*if(n>0){
               // this is to print just the minimum values of F and to ignore distances of the same particle
 	      if(F[j]<F[j-1] & (D[j+p*n_points] !=0) & (D[j+q*n_points] != 0)){
